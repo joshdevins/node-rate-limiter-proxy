e4636ee07b024d773f99d8b54ecb71c7b8266e08
Josh Devins <info@joshdevins.net>
2010-11-20 13:38:23 +0100

    Added some nicer methods, fixed port parsing from original request

--- http-api-limiter.js
+++ http-api-limiter.js
@@ -2,7 +2,11 @@
  * A very simple node.js HTTP proxy providing API usage control using Redis. For now, this is intended
  * to be forked to change the configuration and what parts of the HTTP request are used as API keys.
  *
- * Sources: https://github.com/pkrumins/nodejs-proxy
+ * TODO:
+ *  - test HTTPS support
+ *  - support setting the proxied request remote address to the same as the originating remote address (avoids need for X-Forwarded-For header)
+ *
+ * Sources: https://github.com/pkrumins/nodejs-proxy (proxying code)
  *
  * Author: Josh Devins (info@joshdevins.net, http://joshdevins.net)
  */
@@ -10,18 +14,42 @@
 var sys = require("sys"),
     http = require("http"),
     url = require("url");
+var redis = require("./lib/redis-client.js");
 
 var config = {
     proxy_port: 8080
 };
 
 /**
+ * Provided a key, lookup in Redis what the API usage is and pass through if under limit. If over or at limit,
+ * return an error code to the user.
+ */
+function lookupKeyAndProxy(request, response, key) {
+
+    sys.log("API usage lookup by key: " + key);
+
+    sys.log("Proxying request for key: " + key);
+    proxy(request, response);
+}
+
+/**
  * Proxy the request to the destination service.
  */
 function proxy(request, response) {
-    
-    var host = request.headers['host'].split(':');
-    var proxy = http.createClient(host[1] || 80, host[0])
+
+    // sys.puts(sys.inspect(request));
+
+    var hostSplit = request.headers.host.split(':');
+    var host = hostSplit[0];
+    var port = hostSplit[1];
+
+    if (port == undefined) {
+        port = {false : 80, true : 443}[request.socket.secure]
+    }
+
+    sys.log("Proxying to: " + host + ":" + port);
+
+    var proxy = http.createClient(port, host, request.socket.secure);
     var proxy_request = proxy.request(request.method, request.url, request.headers);
 
     proxy_request.addListener('response', function(proxy_response) {
@@ -51,10 +79,12 @@ function serverCallback(request, response) {
     var auth = request.headers.authorization;
     var method = request.method;
     var uri = url.parse(request.url).pathname;
+    
+    var key = auth + "|" + method + "|" + uri;
 
-    proxy(request, response);
+    lookupKeyAndProxy(request, response, key);
 }
 
 // startup server
-sys.log("Starting HTTP proxy server on port: " + config.proxy_port);
+sys.log("Starting HTTP API limiter proxy server on port: " + config.proxy_port);
 http.createServer(serverCallback).listen(config.proxy_port);
