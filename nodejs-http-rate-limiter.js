a1e68e418057db54fdefd8b9bf9e164dc47fc8a0
Josh Devins <info@joshdevins.net>
2010-11-20 23:57:19 +0100

    Almost working but hanging on proxy call

--- nodejs-http-rate-limiter.js
+++ nodejs-http-rate-limiter.js
@@ -25,9 +25,6 @@ function lookupKeyAndProxy(request, response, key) {
     
     var keyX = "X:" + key;
     var keyY = "Y:" + key;
-    
-    var x = 0;
-    var y = -1;
 
     // increment X and check TTL on Y
     redisClient.multi()
@@ -35,53 +32,48 @@ function lookupKeyAndProxy(request, response, key) {
         .ttl(keyY)
         .exec(function (err, replies) {
 
-            sys.log("MULTI got " + replies.length + " replies");
-
-            replies.forEach(function (reply, index) {
-                sys.log("Reply " + index + ": " + reply.toString());
-            });
-            
             x = replies[0];
             y = replies[1];
+
+            sys.log("X: " + x);
+            sys.log("Y: " + y);
+            
+            // case 1: TTL is expired, need to set X and Y
+            if (y == -1) {
+                redisClient.multi()
+                    .set(keyX, 1)
+                    .setex(keyY, config.periodInSeconds, 0)
+                    .exec();
+
+                sys.log("TTL expired, proxying request for key: " + key);
+                proxy(request, response);
+                return;
+            }
+
+            // case 2: TTL is not expired, verify we are under the limit of maxRequests
+            if (x > config.maxRequests) {
+
+                // rate limit reached
+                sys.log("Usage rate limit hit: " + key);
+
+                response.writeHead(403, {
+                    'X-RateLimit-MaxRequests' : config.maxRequests,
+                    'X-RateLimit-Requests' : x,
+                    'X-RateLimit-TTL' : y });
+                response.end();
+
+                return;
+            }
+
+            sys.log("TTL not expired, number of requests under limit, proxying request for key: " + key);
+            proxy(request, response);
         });
-            
-    sys.log("X: " + x);
-    sys.log("Y: " + y);
-        
-    // case 1: TTL is expired, need to set X and Y
-    if (y != -1) {
-        redisClient.multi()
-            .set(keyX, 1)
-            .setex(keyY, config.periodInSeconds, 0)
-            .exec();
-            
-        sys.log("TTL expired, proxying request for key: " + key);
-        proxy(request, response);
-        return;
-    }
-        
-    // case 2: TTL is not expired, verify we are under the limit of maxRequests
-    if (x > config.maxRequests) {
-
-        // rate limit reached
-        sys.log("Usage rate limit hit: " + key);
-        
-        response.writeHead(403, { 'X-RateLimit-Hit' : '' });
-        response.end();
-
-        return;
-    }
-    
-    sys.log("TTL not expired, under limit, proxying request for key: " + key);
-    proxy(request, response);
 }
 
 /**
  * Proxy the request to the destination service.
  */
 function proxy(request, response) {
-
-    // sys.puts(sys.inspect(request));
 
     var hostSplit = request.headers.host.split(':');
     var host = hostSplit[0];
@@ -97,42 +89,44 @@ function proxy(request, response) {
     var proxy_request = proxy.request(request.method, request.url, request.headers);
 
     proxy_request.addListener('response', function(proxy_response) {
-
-      proxy_response.addListener('data', function(chunk) {
-        response.write(chunk, 'binary');
-      });
-
-      proxy_response.addListener('end', function() {
-        response.end();
-      });
-
-      response.writeHead(proxy_response.statusCode, proxy_response.headers);
+        
+        proxy_response.addListener('data', function(chunk) {
+            response.write(chunk, 'binary');
+        });
+      
+        proxy_response.addListener('end', function() {
+            response.end();
+        });
+      
+        response.writeHead(proxy_response.statusCode, proxy_response.headers);
     });
-
+    
     request.addListener('data', function(chunk) {
-      proxy_request.write(chunk, 'binary');
+        proxy_request.write(chunk, 'binary');
     });
-
+    
     request.addListener('end', function() {
-      proxy_request.end();
+        proxy_request.end();
     });
 }
 
 function serverCallback(request, response) {
 
     var auth = request.headers.authorization;
-    var method = request.method;
-    var uri = url.parse(request.url).pathname;
+    //var method = request.method;
+    //var uri = url.parse(request.url).pathname;
     
-    var key = auth + "|" + method + "|" + uri;
+    var key = auth;
 
     lookupKeyAndProxy(request, response, key);
+    
+    sys.log("Done serverCallback");
 }
 
 // create Redis client
 var redisClient = redis.createClient(6379, 'localhost');
 redisClient.on("error", function (err) {
-    sys.log("Redis connection error to " + client.host + ":" + client.port + " - " + err);
+    sys.log("Redis connection error to " + redisClient.host + ":" + redisClient.port + " - " + err);
 });
 
 // startup server
